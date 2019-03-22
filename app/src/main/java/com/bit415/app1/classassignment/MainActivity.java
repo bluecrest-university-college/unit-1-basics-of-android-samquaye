e3a000c33762350f8a49787a1a9edf1e501c4d1c
Samuel K. Quaye <tma.skquaye@gha.african-agency.com>
2019-03-22 11:35:36 +0000

    Initial commit

---
+++ app/src/main/java/com/bit415/app1/classassignment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bit415.app1.classassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
